b5c540888173427f6f5fa5888c88c28b63faa726
FelipesVargas <felipesouza_16@hotmail.com>
2020-04-01 01:12:42 -0300

    Exercicio concluído, não comentado

--- Exercicio 2/Jogo da Velha - MinMax/Jogo da velha.py
+++ Exercicio 2/Jogo da Velha - MinMax/Jogo da velha.py
@@ -1,8 +1,15 @@
-## Exercicio Jogo da Velha MinMax
-## Autor: Felipe Souza Vargas
-## Exercício: Como obter 2 litros de água no jarro 4.
-## Saída: Lista de estados necessários para se obter 2 litros no jarro de 4.
-#Posições das casa
+#####################################################################################
+## Exercicio Jogo da Velha                                                         ##  
+## Autor: Felipe Souza Vargas                                                      ##
+#####################################################################################
+## Exercício: Jogo da Velha utilizando Algoritmo MinMax para IA do computador.     ##
+## Saída: A saida consite das respostas do computados as jogadas de entrada.       ##
+##       Sendo que com previsão de uma jogada para frente não é possível ganhar    ##
+##       do computador.                                                            ## 
+#####################################################################################
+
+
+#Posições das casas
 #_1_|_2_|_3_
 #_4_|_5_|_6_
 # 7 | 8 | 9 
@@ -14,11 +21,12 @@
 # 2 | 1 | 2 
 
 import math
+import time
 
 maquina=""
 pesos=(2,1,2,1,3,1,2,1,2)
-#      1  2  3  4  5  6  7  8  9
-jogo=["","","","","","","","",""]
+#      1   2   3   4   5   6   7   8   9
+jogo=[" "," "," "," "," "," "," "," "," "]
 
 
 def valor_jogada(estado):
@@ -53,13 +61,13 @@ def proximas_Jogadas(estado, atual):
     if(atual == "O"):
         for i in range(len(estado)):
             novo_estado=estado.copy()
-            if(estado[i] == "" ):
+            if(estado[i] == " " ):
                 novo_estado[i]="O"
                 estados.append(novo_estado)
     else:
         for i in range(len(estado)):
             novo_estado=estado.copy()
-            if(estado[i] == "" ):
+            if(estado[i] == " " ):
                 novo_estado[i]="X"
                 estados.append(novo_estado)
 
@@ -89,11 +97,55 @@ def max_value(estado, atual):
 
     return y[0]
 
-#    retorn valor_jogada(estado)
+def mostrar_jogo(jogo):
+    print(" "+jogo[0]+" | "+jogo[1]+" | "+jogo[2]+"  ")
+    print("-----------")
+    print(" "+jogo[3]+" | "+jogo[4]+" | "+jogo[5]+"  ")
+    print("-----------")
+    print(" "+jogo[6]+" | "+jogo[7]+" | "+jogo[8]+"  ")
+
+def mostrar_velha():
+    print("")
+    print("jogo atual:"+ " "+jogo[0]+" | "+jogo[1]+" | "+jogo[2]+"  "+ "Referencia posicoes"  +"    0 | 1 | 2 ")
+    print("           "+ "-----------"                               + "                   "  +"    -----------")
+    print("           "+ " "+jogo[3]+" | "+jogo[4]+" | "+jogo[5]+"  "+ "                   "  +"    3 | 4 | 5 ")
+    print("           "+ "-----------"                               + "                   "  +"    -----------")
+    print("           "+ " "+jogo[6]+" | "+jogo[7]+" | "+jogo[8]+"  "+ "                   "  +"    6 | 7 | 8 ")
+    print("")
 
 
-jogada = "X"
-maquina = "X"
 
-#print(valor_jogada(jogo))
-print(max_value(jogo, jogada))
+print("Bem vindo ao Jogo da Velha")
+jogador=input("Insira o simbolo que voce deseja jogar (X ou O): ")
+ 
+if(jogador == "X"):
+    maquina="O"
+else:
+    maquina="X"
+
+empate=0
+placar=0
+
+while(empate<5):
+    empate+=1
+    mostrar_velha()
+    jogada=input("Deseja colocar o "+jogador+" em qual posição: ")
+    
+    jogo[int(jogada)]=jogador
+    
+    placar=valor_jogada(jogo)
+    if(placar==math.inf or placar==-math.inf):
+        print("Voce ganhou!!!")
+        empate=9   
+
+    print("Vez do computador...")
+    time.sleep(1)
+    jogo=max_value(jogo, maquina)
+    placar=valor_jogada(jogo)
+    mostrar_velha()
+    if(placar==math.inf or placar==-math.inf):
+        print("Voce perdeu!!!")
+        empate=9      
+
+  
+print("Empate, tente novamente!!")
